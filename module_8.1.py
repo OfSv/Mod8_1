01a90975a5916a64cc619a1c1b3d24ae242ac7fc
OfSv <konkovocso@gmail.com>
2024-12-02 16:55:52 +0300

    Try и Except

---
+++ module_8.1.py
@@ -0,0 +1,14 @@
+# "Try и Except"
+# "Программистам всё можно"
+
+def add_everything_up(a, b):
+    try:
+        return round(a+b, 13)  
+    except TypeError:
+        return f"{a} {b}"
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
